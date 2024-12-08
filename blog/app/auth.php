699bba911956058eca1ecff97d02e520e7ee04b1
tacheraSasi <tacherasasi@gmail.com>
2024-12-08 21:23:10 +0300

    register method

--- blog/app/auth.php
+++ blog/app/auth.php
@@ -1,4 +1,5 @@
 <?php 
+require_once "app.php";
 require_once "db.php";
 
 class Auth extends Database{
@@ -9,7 +10,9 @@ class Auth extends Database{
     public static function register($username, $email, $password){
         $sql = "INSERT INTO users (username, email,password) VALUES (:name, :email ,:password)";
         $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-        self::query($sql,['name'=> $username,'email'=> $email,'password'=> $hashed_password]);
+        if (self::query($sql,['name'=> $username,'email'=> $email,'password'=> $hashed_password])){
+            App::redirect('login.php');
+        }
     }
     public static function logout(){}
 
---
+++ blog/app/autoload.php
@@ -0,0 +1,4 @@
+<?php 
+
+require_once "app.php";
+require_once "db.php";
