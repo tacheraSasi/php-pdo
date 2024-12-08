fcc26426972a4ef3a316ed327ec11a6fd062db6e
tacheraSasi <tacherasasi@gmail.com>
2024-12-08 21:53:52 +0300

    login

--- blog/login.php
+++ blog/login.php
@@ -1,21 +1,12 @@
 <?php
-require 'db.php';
-session_start();
+require 'auth.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $stmt = $pdo->prepare("SELECT * FROM users WHERE email = :email");
-    $stmt->execute(['email' => $email]);
-    $user = $stmt->fetch();
+    Auth::login($email, $password);
 
-    if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['user_id'] = $user['id'];
-        header('Location: index.php');
-    } else {
-        echo "Invalid email or password!";
-    }
 }
 ?>
 
